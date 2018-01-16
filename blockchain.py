e8f9958f8cef2ad539e832c42ca858be5f225d7a
Tim Romero <tim@Tims-MacBook-Pro.local>
2018-01-16 15:23:15 +0900

    Implemented new_block and a crappy hash function

--- blockchain.py
+++ blockchain.py
@@ -1,22 +1,25 @@
 import time
+import hashlib
 
 class Blockchain:
-     __init__(object):
+    def __init__(self):
         self.chain = []
         self.current_transactions = []
 
         # Create the genesis block
-        new_block(0)
+        self.new_block(0)
 
-    def new_transaction(self, from, to, amount):
+    def new_transaction(self, sender, receiver, amount):
         '''Adds a transaction to the current block and returns the index of the
         block that will contain the transaction.'''
         self.current_transactions.append({
-            'from': from,
-            'to': to,
+            'sender': sender,
+            'receiver': receiver,
             'amount': amount
         })
-        return self.last_block['index'] +1
+
+        #TODO maybe don't send retirn value
+        return self.last_block()['index'] + 1
 
 
     def new_block(self, proof):
@@ -24,19 +27,19 @@ class Blockchain:
 
         if len(self.chain) == 0:
             # Genisis block
-            previous_hash = 0
+            self.previous_hash = 0
         else:
-            previous_hash = self.hash(last_block())
+            self.previous_hash = self.hash(self.last_block())
 
+        #TODO time() is only time compent. Need some kind of datetime
         block = {
             'index': len(self.chain) + 1,
             'timestamp': time.time(),
-            'transactions:', self.current_transactions,
-            'proof:', proof,
-            'previous_hash': previous_hash
+            'transactions': self.current_transactions,
+            'proof': proof,
+            'previous_hash': self.previous_hash
         }
 
-        # TODO does this mutate the value in the dict??
         self.current_transactions = []
         self.chain.append(block)
         return block
@@ -45,7 +48,12 @@ class Blockchain:
         '''Returns the last bloch on the chain'''
         return self.chain[-1]
 
+
     # TODO does python support static methods??
-    def hash(block):
-        # TODO check out python cryto libs
-        pass
+    def hash(self, block):
+        # Can't hash objects or unicode strings
+        # TODO This is a very fucked up hash fundtion
+        # Need something more robest. Can't trust cascading implemetntations of __repr__
+        strz = repr(block).encode('utf-8')
+        hash_val = hashlib.sha256(strz).hexdigest()
+        return hash_val
--- test_blockchain.py
+++ test_blockchain.py
@@ -1,9 +1,14 @@
 import pytest
 from blockchain import Blockchain
 
-#@pytest.fixture
-#def empty_blockchain():
-#    return Blockchain()
+
+@pytest.fixture
+def loaded_blockchain():
+    blockchain = Blockchain()
+    blockchain = Blockchain()
+    assert blockchain.new_transaction('foo', 'bar', 10) == 2
+    assert blockchain.new_transaction('a-foo', 'a-bar', 2) == 2
+    return blockchain
 
 
 def test_setup():
@@ -11,6 +16,30 @@ def test_setup():
     assert len(blockchain.chain) == 1
     assert len(blockchain.current_transactions) == 0
 
-    block = blockchain.last_block()
+
+def test_last_block(loaded_blockchain):
+    block = loaded_blockchain.last_block()
     assert block['index'] == 1
     assert len(block['transactions']) == 0
+
+
+def test_new_transaction(loaded_blockchain):
+    assert len(loaded_blockchain.current_transactions) == 2
+
+    assert loaded_blockchain.current_transactions[0]['sender'] == 'foo'
+    assert loaded_blockchain.current_transactions[0]['receiver'] == 'bar'
+    assert loaded_blockchain.current_transactions[0]['amount'] == 10
+
+    assert loaded_blockchain.current_transactions[1]['sender'] == 'a-foo'
+    assert loaded_blockchain.current_transactions[1]['receiver'] == 'a-bar'
+    assert loaded_blockchain.current_transactions[1]['amount'] == 2
+
+
+
+def test_new_block(loaded_blockchain):
+    block = loaded_blockchain.new_block("fooHash")
+    assert block == loaded_blockchain.last_block()
+    assert block['index'] == 2
+    assert len(block['transactions']) == 2
+    assert block['proof'] == "fooHash"
+    assert block['previous_hash']
