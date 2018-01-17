5e9de28e353b88accd929052a94eec012fba2e94
Tim Romero <tim@Tims-MacBook-Pro.local>
2018-01-17 15:30:50 +0900

    Basic PoW implementation.

--- proof.py
+++ proof.py
@@ -1,16 +1,16 @@
 import hashlib
 
-def proof_of_work(last_proof):
+def proof_of_work(previous_block_hash):
     proof = 0
-    while not validate_proof_of_work(proof, last_proof)
+    while not validate_proof_of_work(proof, previous_block_hash):
         proof += 1
 
     return proof
 
 
-def validate_proof_of_work(proof, last_proof):
+def validate_proof_of_work(proof, previous_block_hash):
 
-    challenge_str = "{}{}".format(last_proof, proof)
-    hash_val = hashlib.sha256(challenge_str).hexdigest()
-
-    pass
+    hash_stub = previous_block_hash[-20:]
+    challenge_bytes = f"{previous_block_hash}{proof}".encode('utf-8')
+    hash_val = hashlib.sha256(challenge_bytes).hexdigest()
+    return hash_val[:4] == '0000'
--- test_proof.py
+++ test_proof.py
@@ -1,10 +1,16 @@
 import hashlib
+import proof
 
 
 
 def test_proof_of_work():
-    pass
+    last_hash = hashlib.sha256("FooBar".encode('utf-8')).hexdigest()
+    assert proof.proof_of_work(last_hash) == 270743
 
 
 def test_validate_proof_of_work():
-    pass
+    last_hash = hashlib.sha256("FooBar".encode('utf-8')).hexdigest()
+    print(last_hash)
+
+    assert not proof.validate_proof_of_work(5, last_hash)
+    assert proof.validate_proof_of_work(270743, last_hash)
